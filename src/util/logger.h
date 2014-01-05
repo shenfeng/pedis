ec20ec62e42ff04bead9efaf11f52ba4a993afb6
feng <shenedu@gmail.com>
2014-01-05 10:54:12 +0800

    follow Google C++ Style Guide

--- src/util/logger.h
+++ src/util/logger.h
@@ -1,5 +1,5 @@
-#ifndef _UTIL_LOG_H
-#define _UTIL_LOG_H
+#ifndef _PEDIS_UTIL_LOG_H
+#define _PEDIS_UTIL_LOG_H
 
 #include <assert.h>
 #include <thread>
@@ -14,35 +14,25 @@
 
 namespace pedis {
     class Logger {
-    private:
-        int _level;
-        int _next_rotate_mill;
-        int _rotateHour;
-        FILE *_logfile;
-        std::mutex _mutex;
-        std::string _filename;
-        // manage the memory. No free get called
-        static __thread char* _thread_name; // thread local
-
     public:
-        enum { L_TRACE, L_DEBUG, L_INFO, L_WARN, L_ERROR, L_FATAL };
+        enum { kLevelTrace, kLevelDebug, kLevelInfo, kLevelWarn, kLevelError, kLevelFatal };
 
         // default log to stdout
-        Logger(): _logfile(stdout), _level(L_INFO), _next_rotate_mill(0) {}
-        ~Logger() { std::lock_guard<std::mutex> _(_mutex); _close(); }
+        Logger(): logfile_(stdout), level_(kLevelInfo), next_rotate_mill_(0) {}
+        ~Logger() { std::lock_guard<std::mutex> _(mutex_); this->Close(); }
 
         // Open a logger, filename can be "stdout". rotate every rotateHour
-        bool Open(const std::string &filename, int level=L_INFO, int rotateHour=0);
+        bool Open(const std::string &filename, int level=kLevelInfo, int rotatehour=0);
 
         // set thread name, like log4j, can logout thead name
         // if not set for current thread, use std::this_thread::get_id()
         void SetThreadName(const std::string &name) {
             char* _name = (char *)malloc(name.size() + 1);
             strcpy(_name, name.data());
-            _thread_name = _name;
+            thread_name_ = _name;
         }
 
-        bool Enabled(int level) { return level >= _level; }
+        bool Enabled(int level) { return level >= level_; }
         void Logv (int level, const char* fmt, va_list ap);
         void Logv (int level, const char* fmt, ...) {
             va_list ap;
@@ -52,45 +42,53 @@ namespace pedis {
         }
 
         int SetLevel(int level) {
-            assert(level <= L_FATAL && level >= L_TRACE && "logger level out of range");
-            int old = _level;
-            _level = level;
+            assert(level <= kLevelFatal && level >= kLevelTrace && "logger level out of range");
+            int old = level_;
+            level_ = level;
             return old;
         }
 
     private:
-        Logger(const Logger&);         // No copying allowed
-        void operator=(const Logger&);
+        int level_;
+        int next_rotate_mill_;
+        int rotate_hour_;
+        FILE *logfile_;
+        std::mutex mutex_;
+        std::string filename_;
+        // manage the memory. No free get called
+        static __thread char* thread_name_; // thread local. take ownership of the memory
 
-        inline long millitime(struct timeval const *now) {
-            return  now->tv_sec + now->tv_usec/1000.0/1000;
+        inline long MilliTime(struct timeval const *now) {
+            return now->tv_sec + now->tv_usec/1000.0/1000;
         }
 
         // prerequisite: mutex held
-        inline void _close() {
-            if (_logfile && _logfile != stdout) { fclose(_logfile); }
+        inline void Close() {
+            if (logfile_ && logfile_ != stdout) { fclose(logfile_); }
         }
 
-        inline void try_rotate(struct timeval const *now) {
-            /*
-              first go a fast path by checking _next_rotate_mill, then held the lock.
-              the cost is the log file may get closed then reopened a few times, which is fine
-             */
-            if (_next_rotate_mill > 0 && millitime(now) > _next_rotate_mill) {
-                std::lock_guard<std::mutex> _(_mutex);
-                _close();
+        inline void TryRotate(struct timeval const *now) {
+            // First go a fast path by checking next_rotate_mill_, then grab the lock.
+            // The cost is the log file may get closed then reopened a few times.
+            // which should happen rarely
+            if (next_rotate_mill_ > 0 && this->MilliTime(now) > next_rotate_mill_) {
+                std::lock_guard<std::mutex> _(mutex_);
+                this->Close();
                 char buf[64];
                 strftime(buf,sizeof(buf),"_%F_%H",localtime(&(now->tv_sec)));
-                auto filename = _filename + buf;
-                _logfile = fopen(filename.data(), "a");
-                _next_rotate_mill = millitime(now) + _rotateHour * 60 * 60 * 1000;
+                auto filename = filename_ + buf;
+                logfile_ = fopen(filename.data(), "a");
+                next_rotate_mill_ = this->MilliTime(now) + rotate_hour_ * 60 * 60 * 1000;
             }
         }
+
+        Logger(const Logger&);         // No copying allowed
+        void operator=(const Logger&);
     };
 
     // convenient methods, operate on a global Logger(static)
     void log_write(int level, const char *fmt, ...);
-    bool log_open(const std::string &filename, int level=Logger::L_INFO, int rotateHour=0);
+    bool log_open(const std::string &filename, int level=Logger::kLevelInfo, int rotateHour=0);
     int set_log_level(int level);
     void set_thread_name(const std::string &name);
 
@@ -103,17 +101,17 @@ namespace pedis {
     */
 
 #define log_trace(fmt, args...)                                         \
-    log_write(Logger::L_TRACE, "%s(%d): " fmt, __FILE__, __LINE__, ##args)
+    log_write(Logger::kLevelTrace, "%s(%d): " fmt, __FILE__, __LINE__, ##args)
 #define log_debug(fmt, args...)                                         \
-    log_write(Logger::L_DEBUG, "%s(%d): " fmt, __FILE__, __LINE__, ##args)
+    log_write(Logger::kLevelDebug, "%s(%d): " fmt, __FILE__, __LINE__, ##args)
 #define log_info(fmt, args...)                                          \
-    log_write(Logger::L_INFO,  "%s(%d): " fmt, __FILE__, __LINE__, ##args)
-#define log_warn(fmt, args...)	\
-    log_write(Logger::L_WARN,  "%s(%d): " fmt, __FILE__, __LINE__, ##args)
-#define log_error(fmt, args...)	\
-    log_write(Logger::L_ERROR, "%s(%d): " fmt, __FILE__, __LINE__, ##args)
-#define log_fatal(fmt, args...)	\
-    log_write(Logger::L_FATAL, "%s(%d): " fmt, __FILE__, __LINE__, ##args)
-}
+    log_write(Logger::kLevelInfo,  "%s(%d): " fmt, __FILE__, __LINE__, ##args)
+#define log_warn(fmt, args...)                                          \
+    log_write(Logger::kLevelWarn,  "%s(%d): " fmt, __FILE__, __LINE__, ##args)
+#define log_error(fmt, args...)                                         \
+    log_write(Logger::kLevelError, "%s(%d): " fmt, __FILE__, __LINE__, ##args)
+#define log_fatal(fmt, args...)                                         \
+    log_write(Logger::kLevelFatal, "%s(%d): " fmt, __FILE__, __LINE__, ##args)
+} // namespace pedis
 
-#endif /* _UTIL_LOG_H */
+#endif /* _PEDIS_UTIL_LOG_H */
