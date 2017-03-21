08e0d7e2d55acde1ca049cd06b8e4985b05a6f89
Feng Shen <shenedu@gmail.com>
2017-03-21 17:58:33 +0800

    finally compiles

--- deps/deps.sh
+++ deps/deps.sh
@@ -6,7 +6,7 @@ set -u
 
 if [ ! -d deps/rocksdb ]; then
     echo "cloning rocksdb from github"
-    (mkdir -p deps && cd deps && git clone git@github.com:facebook/rocksdb.git && cd rocksdb && make static_lib -j4)
+    (mkdir -p deps && cd deps && git clone https://github.com/facebook/rocksdb.git && cd rocksdb && make static_lib -j4)
 else
     echo "update rocksdb from github"
 #    (cd deps/rocksdb && git pull && make static_lib -j4)
