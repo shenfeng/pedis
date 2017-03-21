1b21b4e2b746b55c1dbf6943b722ad4781a7e153
Feng Shen <shenedu@gmail.com>
2017-03-21 17:10:21 +0800

    add deps/deps.sh

---
+++ deps/deps.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+
+if [ ! -d deps/rocksdb ]; then
+    echo "cloning rocksdb from github"
+    (mkdir -p deps && cd deps && git clone git@github.com:facebook/rocksdb.git && cd rocksdb && make static_lib -j4)
+else
+    echo "update rocksdb from github"
+#    (cd deps/rocksdb && git pull && make static_lib -j4)
+fi
+
+
+(cd deps && rm -rf gen-cpp && thrift -gen cpp ../api.thrift)
