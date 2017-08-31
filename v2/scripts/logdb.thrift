d1b0dd7b0b052c43af34f540147bd1974763bf14
Feng Shen <shenedu@gmail.com>
2017-08-31 15:41:34 +0800

    update scripts/logdb.thrift

--- v2/scripts/logdb.thrift
+++ v2/scripts/logdb.thrift
@@ -1,36 +1,41 @@
 namespace java logdb.thrift
 
 enum Action {
-        CHAT,
-        CHAT_AUDIO,
-        CHAT_FACETIME,
-        CHAT_INTERVIEW_ACCEPT,
-        CHAT_INTERVIEW_CANCEL,
-        CHAT_INTERVIEW_INVITE,
-        CHAT_INTERVIEW_REFUSE,
-        CHAT_MOBILE,
-        CHAT_MOBILE_ACCEPT,
-        CHAT_MOBILE_REJECT,
-        CHAT_PIC,
-        CHAT_RESUME_GET,
-        CHAT_RESUME_GET_ACCEPT,
-        CHAT_RESUME_GET_REJECT,
-        CHAT_RESUME_SEND_ACCEPT,
-        CHAT_WEIXIN,
-        CHAT_WEIXIN_ACCEPT,
-        CHAT_WEIXIN_REJECT,
-        DETAIL_BOSS,
-        DETAIL_BRAND,
-        DETAIL_GEEK,
-        DETAIL_GEEK_ADDFRIEND,
-        LIST_BOSS,
-        LIST_BOSS_SEARCH,
-        LIST_GEEK,
-        LIST_GEEK_SEARCH,
-        LIST_NOTIFY,
-        OPEN_RESUME,
-        SEARCH_CLICK,
-        SEARCH_LIST,
+CHAT
+CHAT_AUDIO
+CHAT_FACETIME
+CHAT_INTERVIEW_ACCEPT
+CHAT_INTERVIEW_CANCEL
+CHAT_INTERVIEW_INVITE
+CHAT_INTERVIEW_REFUSE
+CHAT_MOBILE
+CHAT_MOBILE_ACCEPT
+CHAT_MOBILE_REJECT
+CHAT_PIC
+CHAT_RESUME_GET
+CHAT_RESUME_GET_ACCEPT
+CHAT_RESUME_GET_REJECT
+CHAT_RESUME_SEND
+CHAT_RESUME_SEND_ACCEPT
+CHAT_WEIXIN
+CHAT_WEIXIN_ACCEPT
+CHAT_WEIXIN_REJECT
+DETAIL_BOSS
+DETAIL_BRAND
+DETAIL_F_BOSS
+DETAIL_F_GEEK
+DETAIL_GEEK
+DETAIL_GEEK_ADDFRIEND
+GEEK_CALL
+GEEK_CALL_USED
+LIST_BOSS
+LIST_BOSS_SEARCH
+LIST_GEEK
+LIST_GEEK_SEARCH
+LIST_NOTIFY
+OPEN_RESUME
+SEARCH_CLICK
+SEARCH_LIST
 }
 
 struct LogItem {
@@ -43,11 +48,13 @@ struct LogItem {
     5: optional i32 geekId,
     6: optional i32 expectId
 
-    7: optional string msg  // 聊天内容
-    8: optional string algo // detail_geek 等的算法标记
-    9: optional string keyword
+    7: optional string msg     // 聊天内容
+    8: optional string algo    // detail_geek 等的算法标记
+    9: optional string keyword // 搜索关键字
     10: optional string filter // 搜索的筛选条件
     11: optional string extra
+    12: optional i16 page      // list-geek-search, list-boss, list-geek, list-boss-search等
+    13: optional i32 brandId
 }
 
 
